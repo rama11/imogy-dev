0bb2fec7ab53787f2a2ba75a9e1ed6a15750e7f2
rama11 <prof.agastyo@gmail.com>
2019-10-03 11:23:05 +0700

    testing for firebase

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -739,19 +739,20 @@ class TestController extends Controller
 			->create();
 
 		$database = $firebase->getDatabase();
-		$newPost = $database
-			->getReference('blog/posts')
-			->push([
-				'title' => 'Post title',
-				'body' => 'This should probably be longer.'
-			]);
-		//$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
-		//$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
-		//$newPost->getChild('title')->set('Changed post title');
-		//$newPost->getValue(); // Fetches the data from the realtime database
-		//$newPost->remove();
-		echo"<pre>";
-		print_r($newPost->getvalue());
+		return $database;
+		// $newPost = $database
+		// 	->getReference('blog/posts')
+		// 	->push([
+		// 		'title' => 'Post title',
+		// 		'body' => 'This should probably be longer.'
+		// 	]);
+		// //$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+		// //$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+		// //$newPost->getChild('title')->set('Changed post title');
+		// //$newPost->getValue(); // Fetches the data from the realtime database
+		// //$newPost->remove();
+		// echo"<pre>";
+		// print_r($newPost->getvalue());
 	}
 
 }

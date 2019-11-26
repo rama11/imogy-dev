57ad1cd041a6b227291388cfb5b05573c6d711cb
rama11 <prof.agastyo@gmail.com>
2019-11-26 09:42:26 +0700

    not use anymore

--- public/download.php
+++ public/download.php
@@ -4,18 +4,22 @@ $dir    =  getcwd() . '/public/';
 $files1 = scandir($dir);
 
 $nameFile = $_GET['nameFile'];
-$attachment_location = $_SERVER["DOCUMENT_ROOT"] . "/report/" . $nameFile;
-if (file_exists($attachment_location)) {
+$nameFile = "Report_BJBR_-_November_2019-11-25_Rama.xlsx";
+$attachment_location = $_SERVER["DOCUMENT_ROOT"] . "report/" . $nameFile;
+// echo $attachment_location;
+// echo file_exists($attachment_location);
+// if (file_exists($attachment_location)) {
 
 	header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
 	header('Content-Disposition: attachment; filename=' . $nameFile );
-	header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+	header('Content-Type: application/octet-stream');
 	header('Content-Length: ' . filesize($nameFile));
 	header('Content-Transfer-Encoding: binary');
 	header('Cache-Control: must-revalidate');
 	header('Pragma: public');
-	readfile($nameFile);
-	die();
-} else {
-	die("Error: File not found.");
-} 
+	flush(); 
+	readfile($attachment_location);
+// 	die();
+// } else {
+// 	die("Error: File not found.");
+// } 

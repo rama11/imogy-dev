2862b376a3f76ace03e895eeeb4aa3b1e66a7c99
rama11 <prof.agastyo@gmail.com>
2019-12-19 11:01:50 +0700

    add controll link

--- resources/views/layouts/superuser/slayout.blade.php
+++ resources/views/layouts/superuser/slayout.blade.php
@@ -17,54 +17,6 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.0/css/AdminLTE.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.0/css/skins/_all-skins.min.css">
 		
-		<style type="text/css">
-			.switch {
-				position: relative;
-				display: inline-block;
-				width: 60px;
-				height: 34px;
-			}
-			.switch input {display:none;}
-			.slider {
-				position: absolute;
-				cursor: pointer;
-				top: 0;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				background-color: #ccc;
-				-webkit-transition: .4s;
-				transition: .4s;
-			}
-			.slider:before {
-				position: absolute;
-				content: "";
-				height: 26px;
-				width: 26px;
-				left: 4px;
-				bottom: 4px;
-				background-color: white;
-				-webkit-transition: .4s;
-				transition: .4s;
-			}
-			input:checked + .slider {
-				background-color: #2196F3;
-			}
-			input:focus + .slider {
-				box-shadow: 0 0 1px #2196F3;
-			}
-			input:checked + .slider:before {
-				-webkit-transform: translateX(26px);
-				-ms-transform: translateX(26px);
-				transform: translateX(26px);
-			}
-			.slider.round {
-				border-radius: 34px;
-			}
-			.slider.round:before {
-				border-radius: 50%;
-			}
-		</style>
 	</head>
 	@if(isset($sidebar_collapse))
 	<body class="hold-transition skin-blue sidebar-mini sidebar-collapse">
@@ -172,6 +124,12 @@
 							<a href="{{ url('tisygy')}}">
 								<i class="fa fa-paper-plane"></i>
 								<span>TISYGY</span>
+							</a>
+						</li>
+						<li class="activeable" id="controll">
+							<a href="{{ url('tisygy/controll')}}">
+								<i class="fa fa-gg"></i>
+								<span>Controll</span>
 							</a>
 						</li>
 						<li class="activeable" id="usermanage">

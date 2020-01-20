c4103d4dadc7d2e9a7c363428095b3383f04738f
rama11 <prof.agastyo@gmail.com>
2020-01-20 13:41:06 +0700

    Add relation note model to activity model

--- app/Http/Models/BudgetNote.php
+++ app/Http/Models/BudgetNote.php
@@ -27,4 +27,13 @@ class BudgetNote extends Model
 	public function account_note(){
 		return $this->hasOne('App\Http\Models\BudgetAccount','id','id_account');
 	}
+
+	public function activity_note(){
+		return $this->hasMany('App\Http\Models\BudgetActivity','id_note','id');
+	}
+
+	public function latest_activity_note(){
+		return $this->hasMany('App\Http\Models\BudgetActivity','id_note','id')
+			->orderBy('budget__activity.id','DESC')->limit(1);;
+	}
 }

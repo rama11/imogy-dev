c2fbcbf7ed43e3136dc09b8fedc2b687533c96ff
rama11 <prof.agastyo@gmail.com>
2020-02-11 15:29:13 +0700

    Try to using test unit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $this->assertDatabaseHas('users', [
+	        'email' => 'agastya@sinergy.co.id'
+	    ]);
     }
 }

55e2cc5b798493f9b824895a6723eff0e020a7cc
rama11 <prof.agastyo@gmail.com>
2020-03-16 16:51:33 +0700

    add service provider

--- config/app.php
+++ config/app.php
@@ -268,6 +268,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Yajra\Datatables\DatatablesServiceProvider::class,
+        Telegram\Bot\Laravel\TelegramServiceProvider::class
 
     ],
 
@@ -317,6 +318,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Telegram' => Telegram\Bot\Laravel\Facades\Telegram::class,
 
     ],
 

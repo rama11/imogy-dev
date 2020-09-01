8283fa8b252494da5e1348f372314f8129d15d75
rama11 <prof.agastyo@gmail.com>
2020-09-01 08:51:33 +0700

    change atm wincor open

--- resources/views/mailOpenTicketWincor.blade.php
+++ resources/views/mailOpenTicketWincor.blade.php
@@ -2,18 +2,14 @@
 <br>
 <p style="font-family: 'calibri' , sans-serif;">Mohon untuk di openkan ticket untuk perbaikan problem pada mesin ATM/CDM berikut ini :</p>
 <br>
-<p style="font-family: 'calibri' , sans-serif;"><b>Name of Bank :</b> <span class="holderCustomer"></span></p>
-<br>
-<p style="font-family: 'calibri' , sans-serif;"><b>Helpdesk/Customer Ticket number :</b> <span class="holderID"></span></p>
-<p style="font-family: 'calibri' , sans-serif;"><b>Machine ID :</b> <span class="holderIDATM"></span></p>
-<p style="font-family: 'calibri' , sans-serif;"><b>Location :</b> <span class="holderLocation"></p>
-<p style="font-family: 'calibri' , sans-serif;"><b>Serial Number :</b> <span class="holderSerial"></p>
-<p style="font-family: 'calibri' , sans-serif;"><b>Machine Type :</b> <span class="holderType"></p>
-<br>
-<p style="font-family: 'calibri' , sans-serif;"><b>Contact Person :</b> <span class="holderPIC"></p>
-<p style="font-family: 'calibri' , sans-serif;"><b>Phone number :</b> <span class="holderContact"></p>
-<br>
-<p style="font-family: 'calibri' , sans-serif;"><b>Problem description :</b> <span class="holderProblem"></p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Name of Bank :</b> <span class="holderCustomer"></span>;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Machine ID :</b> <span class="holderIDATM"></span>;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Location :</b> <span class="holderLocation">;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Serial Number :</b> <span class="holderSerial">;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Machine Type :</b> <span class="holderType">;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Contact Person :</b> <span class="holderPIC">;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Phone number :</b> <span class="holderContact">;</p>
+<p style="font-family: 'calibri' , sans-serif;"><b>Problem description :</b> <span class="holderProblem">;</p>
 <br>
 <br>
 <p style="font-family: 'calibri' , sans-serif;">Best regards,</p>

f10e57a445bcc2c20d945ee816bdcfc1fe9d8359
rama11 <prof.agastyo@gmail.com>
2021-03-19 09:23:28 +0700

    remove space

--- resources/views/tisygy.blade.php
+++ resources/views/tisygy.blade.php
@@ -1251,7 +1251,7 @@
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
-						<h4 class="modal-title" id="modal-ticket-title">Send On Progress Ticket </h4>
+						<h4 class="modal-title" id="modal-ticket-title">Send On Progress Ticket</h4>
 					</div>
 					<div class="modal-body">
 						<div class="form-horizontal">

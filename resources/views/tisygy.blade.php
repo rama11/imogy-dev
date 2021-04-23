16dcc1175d350786e41b1b4fb4a84d507a92fb47
rama11 <prof.agastyo@gmail.com>
2021-04-23 09:23:47 +0700

    add real fotter on ticket blade

--- resources/views/tisygy.blade.php
+++ resources/views/tisygy.blade.php
@@ -1842,7 +1842,7 @@
 	<div class="pull-right hidden-xs">
 		<b>Version</b> 2.4.0
 	</div>
-	<strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+	<strong>Copyright &copy; {{date('Y')}} <a href="https://www.sinergy.co.id">PT. Sinergy Informasi Pratama</a>.</strong> All rights
 	reserved.
 </footer>
 @endsection 

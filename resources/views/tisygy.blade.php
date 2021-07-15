5814916190a926962fde5dfda04fe40f2c9e07be
rama11 <prof.agastyo@gmail.com>
2021-07-15 14:46:30 +0700

    init denny report range button

--- resources/views/tisygy.blade.php
+++ resources/views/tisygy.blade.php
@@ -5372,6 +5372,25 @@
 		}
 	);
 
+	$('#daterange-btn2').daterangepicker(
+		{
+			ranges: {
+				'Today': [moment(), moment()],
+				'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+				'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+				'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+				'This Month': [moment().startOf('month'), moment().endOf('month')],
+				'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+			},
+			startDate: moment().subtract(29, 'days'),
+			endDate: moment()
+		},
+		function (start, end) {
+			$('#daterange-btn2 span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+			$("#ReportingButtonGoNew2").show()
+		}
+	);
+
 	$("#ReportingButtonGoNew").on('click',function(){
 		swalWithCustomClass.fire({
 			title: 'Are you sure?',
